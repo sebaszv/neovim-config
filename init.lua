0c793e3e6bcea02cf80d1a8ea6e55cec03d1ef16
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-03-10 01:58:19 -0400

    Add initial options

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
+require("opts")
 require("plugins")
---
+++ lua/opts.lua
@@ -0,0 +1,7 @@
+vim.opt.hlsearch = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.undofile = true
+vim.opt.updatetime = 250
