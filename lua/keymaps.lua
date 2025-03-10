3a77a9cdc215abb8213740e5689aaafcd8a2a565
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-03-10 02:08:54 -0400

    Add initial keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set(
+  "n",
+  "j",
+  "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "k",
+  "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, silent = true }
+)
