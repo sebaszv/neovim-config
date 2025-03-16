e71338cda0e589cf02ea28becee4f3eae8066904
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-03-16 04:53:28 -0400

    Add gitsigns.nvim

---
+++ lua/plugins/specs/gitsigns.lua
@@ -0,0 +1,8 @@
+-- Documentation: <https://github.com/lewis6991/gitsigns.nvim>
+local M = {
+  "lewis6991/gitsigns.nvim",
+  -- TODO: Configure `on_attach` function for setting up keymaps.
+  opts = { attach_to_untracked = true },
+}
+
+return { M }
