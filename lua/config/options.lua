7c8505fdbc53348c991891cbcfd4acf7327e2644
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-03-26 19:36:27 -0400

    options: disable syncing with system clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- LazyVim has this set to 'unnamedplus' by default.
+-- I prefer accessing the system clipboard through its
+-- respective registers than syncing with the system clipboard.
+vim.opt.clipboard = ""
