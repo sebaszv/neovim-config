bf0ed420df25545730b7bc7f7719613ab877298e
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-03-26 19:37:48 -0400

    lsp: ensure that haskell-language-server isn't installed by Mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,15 @@ return {
       -- that some builds by Mason don't place nicely
       -- with NixOS.
       opts.PATH = "append"
+
+      -- Filter out haskell-language-server so that Mason
+      -- doesn't try to install it. Building it requires
+      -- ghcup, which I don't have globally installed
+      -- on my NixOS systems. Mason is able to use
+      -- whatever is on PATH.
+      opts.ensure_installed = vim.tbl_filter(function(x)
+        return x ~= "haskell-language-server"
+      end, opts.ensure_installed or {})
     end,
   },
 }
