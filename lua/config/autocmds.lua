27481128e64abd9a674c7a6c4725de5d27010d12
Sebastián Zavala Villagómez <sebaszv@proton.me>
2026-07-14 22:49:52 -0400

    autocmds: Synchronise shada when registers are mutated

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,6 +7,39 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Ensures that registers are synchronised between Neovim clients using the same
+-- shada file, which is the default behaviour (so almost always yes unless you
+-- explicitly opt into using another file). This saves the headache of copying
+-- to the system clipboards to share between separate processes. This however
+-- is still beholden to the shada limits (configured through the `shada` option).
+-- Anything that exceeds the limits (which are not modified here) will simply
+-- not be written and left in memory. To not burden with constant large writes
+-- and reads (which would not only incur startup read costs, but midsession since
+-- this is happening live), the limits haven't been changed here. For moving
+-- something larger between sessions the system clipboard will do. A few lines
+-- and macros automatically syncing is already a substantial quality-of-life
+-- improvement. This doesn't mean that the limits cannot be raised to be more
+-- useful whilst remaining sane. That can be explored at a later time.
+local auto_shada_sync_group = vim.api.nvim_create_augroup("auto_shada_sync", { clear = true })
+vim.api.nvim_create_autocmd({ "TextYankPost", "RecordingLeave" }, {
+  group = auto_shada_sync_group,
+  desc = "Write shada after yanking or recording macro (registers updated)",
+  callback = function()
+    -- The shada file can be written before the recorded macro content is
+    -- committed to the register. Deferring the callback to run in the
+    -- next main event-loop iteration guarantees it is updated and ready
+    -- prior to writing the shada file.
+    vim.schedule(function()
+      vim.cmd.wshada()
+    end)
+  end,
+})
+vim.api.nvim_create_autocmd("FocusGained", {
+  group = auto_shada_sync_group,
+  desc = "Read shada when focus gained",
+  command = "rshada",
+})
+
 -- Writing loads of Nix module files that require the same
 -- basic starting template is tedious. This automates that
 -- away.
