a420ea3643ee1a02391c8ee2e953f0199b550be9
Sebastián Zavala Villagómez <sebaszv@proton.me>
2026-07-21 18:12:54 -0400

    luasnip: enable lazy lua snippets loading

---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    optional = true,
+    opts = function()
+      -- Only do this when the extra is enabled since this
+      -- is only useful for intentional LuaSnip use, not
+      -- when a transitive dependency.
+      -- That module also calls the lazy VSCode loader.
+      -- View that extras module to see how it's wired.
+      if LazyVim.has_extra("coding.luasnip") then
+        -- Scan for "luasnippets" at the root of all RTP entries.
+        -- Load children snippets mapped to filetype by module
+        -- name (e.g. `luasnippets/nix.lua` -> `nix`) lazily
+        -- whenever LuaSnip is actually set up. This includes
+        -- `vim.fn.stdpath("config")` since it is an RTP entry.
+        require("luasnip.loaders.from_lua").lazy_load()
+      end
+    end,
+  },
+}
---
+++ luasnippets/README.md
@@ -0,0 +1,5 @@
+# luasnippets
+
+These are loaded dynamically and automatically by `L3MON4D3/LuaSnip`.\
+View [`../lua/plugins/snippets.lua`](../lua/plugins/snippets.lua) to see how
+this is wired.
