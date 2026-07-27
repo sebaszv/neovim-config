abfc8efe6799c990fbdf2f20fb5f1a31393f3dbb
Sebastián Zavala Villagómez <sebaszv@proton.me>
2026-07-27 13:05:32 -0400

    util/treesitter/cursor_at_top_level: Init

--- lua/util/treesitter.lua
+++ lua/util/treesitter.lua
@@ -3,6 +3,35 @@ local win_util = require("util.window")
 ---@class util.treesitter
 local M = {}
 
+--- Whether at the top-level of a buffer.
+---@param root_node_type string Type of parent of all nodes that marks the root of the node tree.
+---@param buf integer? Buffer to check or the current buffer.
+---@param cursor_or_win ([integer, integer] | integer)?  Cursor to check position of. A window ID can be specified instead, whose cursor will be used. Otherwise, the cursor for the current window is used.
+---@return boolean
+function M.cursor_at_top_level(root_node_type, buf, cursor_or_win)
+  local cursor
+
+  if type(cursor_or_win) == "table" then
+    cursor = cursor_or_win
+  else
+    cursor = vim.api.nvim_win_get_cursor(cursor_or_win or 0)
+  end
+
+  local row, col = cursor[1] - 1, cursor[2]
+  local node = vim.treesitter.get_node({
+    bufnr = buf or 0,
+    pos = { row, col },
+  })
+
+  if not node then
+    return true
+  end
+
+  local parent = node:parent()
+
+  return not parent or parent:type() == root_node_type
+end
+
 --- Whether the cursor (or cursor of the window) is positioned inside the treesitter node.
 ---@param ts_node TSNode Treesitter node to check in.
 ---@param cursor_or_win ([integer, integer] | integer)? Cursor to check position of. A window ID can be specified instead, whose cursor will be used. Otherwise, the cursor for the current window is used.
